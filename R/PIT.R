1f12b04d7d0bc111e05689eedb43c0cb59c95017
jethrobrowell <jethrobrowell@gmail.com>
2020-01-29 16:55:12 +0000

    NA handling in PIT...
    Bug fix in commit... mistake not testing it  fully first!!!

--- R/PIT.R
+++ R/PIT.R
@@ -51,7 +51,7 @@ PIT.MultiQR <- function(qrdata,obs,tails,inverse=FALSE,...){
     }
     
     # Impose range [0,1] on PIT transformation and issue warning if used.
-    if((sum(X>1) + sum(X<0))>0){
+    if((sum(na.omit(X)>1) + sum(na.omit(X)<0))>0){
       warning("Boundary [0,1] imposed on PIT transformation. Check tails of marginals.")
       X <- ifelse((X <- ifelse(X<0,0,X))>1,1,X)
     }
