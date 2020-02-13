b6a577d1cf5820beb076d877f3cfaef34c729053
Ciaran Gilbert <41995662+ciaran-g@users.noreply.github.com>
2020-02-13 11:12:44 +0000

    overhauled scenario generation script
    Simplified scenario generation script. (Hopefully) now it's easier to understand and it can deal with multiple sites much better now I think...although there's some changes to the syntax etc. to make it more intuative, in case you are using it in your scripts
    
    Included scenario generation function in example.R for PPD forecasts
    
    renamed "cov.cor_matrix.R" function and included option for the scaling of the gaussian variables before calculation of the covariance matrices
    
    Minor bug fix to contCDF.R in the roxygen2 code

--- R/covcor_matrix.R
+++ R/covcor_matrix.R
@@ -4,17 +4,20 @@
 #' @param u_data A dataframe of uniform distributed variables.
 #' @param kfold A vector of kfold identifiers.
 #' @param cov_cor specify either covariance or correlation
+#' @param scale scale gaussian variable inputs to have unit variance for covariance matrix? Defaults to FALSE.
 #' @details Details go here...
 #' @return A list of covariance/correlation matrices corresponding to kfold ids
 #' @export
-cov.cor_matrix <- function(u_data,kfold=NULL,cov_cor="covariance",use="pairwise.complete.obs",...){
+covcor_matrix <- function(u_data,kfold=NULL,cov_cor="covariance",use="pairwise.complete.obs",scale = F ,...){
   
   ### change to autospecify spatial/spatiotemporal from long format marginals?
   if(is.null(kfold)){
     kfold <- rep(1,nrow(u_data))
   }
   
+  
   g_data <- as.data.frame(sapply(u_data, qnorm))
+  
   
   matList <- list()
   
@@ -22,9 +25,19 @@ cov.cor_matrix <- function(u_data,kfold=NULL,cov_cor="covariance",use="pairwise.
     
     for (fold in unique(kfold)) {
       if(length(unique(kfold))>1){
-        temp <- cov(x = g_data[kfold!=fold & kfold!="Test", ], use=use,...)
+        if(scale){
+          temp <- cov(x = sapply(g_data[kfold!=fold & kfold!="Test",],function(x){x/sd(x,na.rm = T)}), use=use,...)
+          temp[temp>1] <- 1
+        } else{
+          temp <- cov(x = g_data[kfold!=fold & kfold!="Test",], use=use,...)
+        }
       }else{
-        temp <- cov(x = g_data, use=use,...)
+        if(scale){
+          temp <- cov(x = sapply(g_data,function(x){x/sd(x,na.rm = T)}), use=use,...)
+          temp[temp>1] <- 1
+        } else{
+          temp <- cov(x = g_data, use=use,...)
+        }
       }
       matList[[fold]] <- temp
     }
