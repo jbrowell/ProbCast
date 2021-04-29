032a87e1d6adbb8f93e0f96d27f7970b4f1775a4
rosemary-t <62142518+rosemary-t@users.noreply.github.com>
2021-04-29 09:28:27 +0100

    adapted re-writing MQR function to use lightgbm

--- Example.R
+++ Example.R
@@ -51,6 +51,7 @@ test1$gbm_mqr <- qreg_gbm(data = test1$data,
                           only_mqr = TRUE)
 
 
+
 test1$lightgbm_mqr <- qreg_lightgbm(data=test1$data,
                                     formula = TARGETVAR~U100+V100+U10+V10+WS100,
                                     cv_folds = "kfold",
@@ -62,6 +63,26 @@ test1$lightgbm_mqr <- qreg_lightgbm(data=test1$data,
                                     cores=detectCores(),
                                     only_mqr = TRUE)
 
+# data=test1$data
+# formula = TARGETVAR~U100+V100+U10+V10+WS100
+# cv_folds = "kfold"
+# max_depth = 6
+# num_leaves = 63
+# quantiles = seq(0.05,0.95,by=0.05)
+# sort = T
+# sort_limits = list(U=1,L=0)
+# cores=detectCores()
+# only_mqr = TRUE
+# num_iterations=100
+# save_models_path = NULL
+# exclude_train = NULL
+
+
+## for testing predict.qreg_lgbm function..
+# object = output
+# newdata = as.matrix(data[output$kfold_index==fold,features])
+# quantiles = NULL
+# model_name = fold 
 
 par(mar=c(3,3,0.5,1))  # Trim margin around plot [b,l,t,r]
 par(tcl=0.35)  # Switch tick marks to insides of axes
--- R/MQR_lightgbm.R
+++ R/MQR_lightgbm.R
@@ -5,8 +5,8 @@
 #' 
 #' @author Jethro Browell, \email{jethro.browell@@strath.ac.uk}; Ciaran Gilbert, \email{ciaran.gilbert@@strath.ac.uk}; Rosemary Tawn, \email{rosemary.tawn@@strath.ac.uk}
 #' @param data A \code{data.frame} containing target and explanatory variables.
-#' @param formala A \code{formula} object with the response on the left
-#' of an ~ operator, and the terms, separated by + operators, on the right
+#' @param formula A \code{formula} object with the response on the left
+#' of an ~ operator, and the terms, separated by + operators, on the right. All terms should be column names in \code{data}
 #' @param quantiles The quantiles to fit models for.
 #' @param cv_folds Control for cross-validation with various options, either:
 #' \itemize{
@@ -17,14 +17,8 @@
 #'  \item vector of length==nrow(data) containing character or numeric fold labels.
 #'  \item NULL indicates that no cross validation should be performed and the returned model is trained on all \code{data}.
 #' }
-#' @param perf.plot Plot GBM performance?
-#' @param pred_ntree predict using a user-specified tree.
-#' If NULL \code{gbm::gbm.perf()} is used to estimate the best tree via out-of-the-bag estimates,
-#' unless internal gbm cross-validation folds are specified in \code{...}.
-#' @param cores the number of available cores. Defaults to one, i.e. no parallelisation, although in this case the user
-#'  must still specify \code{pckgs} if applicable.
-#' @param pckgs specify additional packages required for
-#' each worker (e.g. c("data.table") if data stored as such).
+#' @param num_iterations Number of boosting iterations supplied to \code{lightgbm}, default 100.
+#' @param cores the number of available cores. Defaults to one, i.e. no parallelisation
 #' @param sort Sort quantiles using \code{SortQuantiles()}?
 #' @param sort_limits \code{Limits} argument to be passed to \code{SortQuantiles()}. Constrains quantiles to upper and 
 #' lower limits given by \code{list(U=upperlim,L=lowerlim)}.
@@ -38,7 +32,7 @@
 #'  \item NULL indicates no exclusion
 #' }
 #' This option is useful when out-of-sample predictions are required in rows which need excluded during model training
-#' @param ... Additional arguments passed to \code{gbm()}.
+#' @param ... Additional arguments passed to \code{lightgbm()}.
 #' @details The returned predictive quantiles are those produced out-of-sample for each
 #' cross-validation fold (using models trained on the remaining folds but not "Test" data).
 #' Predictive quantiles corresponding to "Test" data are produced using models trained on all
@@ -58,10 +52,8 @@ qreg_lightgbm <- function(data,
                      formula,
                      quantiles = c(0.25,0.5,0.75),
                      cv_folds = NULL,
-                     perf.plot = FALSE,
-                     pred_ntree = NULL,
+                     num_iterations=100,
                      cores = 1,
-                     pckgs = NULL,
                      sort = TRUE,
                      sort_limits = NULL,
                      save_models_path = NULL,
@@ -92,9 +84,13 @@ qreg_lightgbm <- function(data,
   output$exclude_index <- exclude_fun(data = data,exclude_train = exclude_train)
   
   
-  # get list of features, and the label (response) column, to later make lightgbm dataset for each fold combination.
+  # get list of features, and the label (response) column, to later make lightgbm dataset.
   features <- labels(terms(formula))
   response <- as.character(formula)[[2]]
+  
+  if(!all(features %in% names(data))){
+    stop("some of the predictors in formula do not have an associated column in data")
+  }
   
 
   # set up parallel workers, defaults to one worker....
@@ -109,38 +105,31 @@ qreg_lightgbm <- function(data,
   
 
   # fit the models: returns list quantile --> kfold
-  output$models <- foreach::foreach(q = quantiles,.packages = c("lightgbm",pckgs),.options.snow = opts) %dopar% {
-    
-    temp_gbm <- list()
+  output$models <- foreach::foreach(q = quantiles,.packages ="lightgbm",.options.snow = opts) %dopar% {
+    temp_lgbm <- list()
     
     for(fold in output$model_names){
       train_data <- data[output$kfold_index!=fold & output$kfold_index!="Test" & output$exclude_index==0 & !is.na(data[[formula[[2]]]]),]
       X <- as.matrix(train_data[, features])
-      dataset <- lightgbm::lgb.Dataset(data=X, label=train_data[,response])
-      lgb_params <- list(..., objective="quantile")
-      lgb_model = lightgbm::lgb.train(params=lgb_params, data=dataset, eval="quantile", alpha=j, num_threads=1) 
+      dataset <- lgb.Dataset(data=X, label=train_data[,response])
+      #lgb_params <- list(..., num_iterations=num_iterations, objective="quantile")
+      lgb_params <- list(objective="quantile", num_iterations=num_iterations)
       
-      temp <- gbm::gbm(formula=formula,
-                       data = data[output$kfold_index!=fold & output$kfold_index!="Test" & output$exclude_index==0 & !is.na(data[[formula[[2]]]]),],
-                       distribution = list(name="quantile",alpha=q),
-                       n.cores=1, # prevent issues when cv.folds>1 are in ...
-                       ...)
-
+      lgb_model = lgb.train(params=lgb_params, data=dataset, alpha=q, num_threads=1)
       
-      # save model?
       if(!is.null(save_models_path)){
-          try(save(temp,file = paste0(save_models_path,"_q",100*q,"_",fold,".rda")))
-
+        try(save(lgb_model,file = paste0(save_models_path,"_q",100*q,"_",fold,".rda")))
       }
       
-      temp_gbm[[fold]] <- temp
-
+      temp_lgbm[[fold]] <- lgb_model
       
-      
-    }
-    
-    return(temp_gbm)
-    
+      }
+      #if('try-error' %in% class(lgb_model)){
+      #  return(paste0("problem fitting model to node ",q))
+      #} else{
+      #  return(lgb_model)
+    return (temp_lgbm)
+  
   }
   
   close(pb)
@@ -158,7 +147,7 @@ qreg_lightgbm <- function(data,
   names(output$models) <- output$model_names
   
   #set new class & default model for prediction
-  class(output) <- "qreg_gbm"
+  class(output) <- "qreg_lightgbm"
   output$default_model <- if("Test"%in%output$model_names){"Test"}else{output$model_names[1]}
   
   
@@ -173,11 +162,9 @@ qreg_lightgbm <- function(data,
     for(fold in output$model_names){
       
       output$mqr_pred[output$kfold_index==fold,] <- predict(output,
-                                                            newdata = data[output$kfold_index==fold,],
+                                                            newdata = as.matrix(data[output$kfold_index==fold,features]),
                                                             quantiles = NULL,
                                                             model_name = fold, 
-                                                            pred_ntree = pred_ntree,
-                                                            perf.plot = perf.plot,
                                                             sort = sort,
                                                             sort_limits = sort_limits)
       
@@ -210,136 +197,20 @@ qreg_lightgbm <- function(data,
 
 
 
-#' Multiple Quantile Regression Using Gradient Boosted Decision Trees (depreciated)
-#'
-#' This function is now depreciated and may be removed in future versions of this package.
-#' Use \code{qreg_gbm()} instead.
-#' 
-#' @author Jethro Browell, \email{jethro.browell@@strath.ac.uk}; Ciaran Gilbert, \email{ciaran.gilbert@@strath.ac.uk}
-#' @param data A \code{data.frame} containing target and explanatory
-#' variables. May optionally contain a collumn called "kfold" with
-#' numbered/labeled folds and "Test" for test data.
-#' @param formala A \code{formula} object with the response on the left
-#' of an ~ operator, and the terms, separated by + operators, on the right
-#' @param quantiles The quantiles to fit models for.
-#' @param gbm_params List of parameters to be passed to \code{fit.gbm()}.
-#' @param CVfolds Control for cross-validation if not supplied in \code{data}.
-#' @param perf.plot Plot GBM performance?
-#' @param pred_ntree predict using a user-specified tree.
-#' If unspecified an out-of-the bag estimate will be used unless interval
-#' gbm cross-validation folds are specified in \code{gbm_params}.
-#' @param parallel \code{boolean} parallelize cross-validation process?
-#' Parallelisation is over cross-validation folds by default, optionally over
-#' quantiles by setting code{para_over_q=T}.
-#' @param pckgs if \code{parallel} is TRUE then  specify packages required for
-#' each worker (e.g. c("data.table) if data stored as such).
-#' @param cores if \code{parallel} is TRUE then number of available cores
-#' @param para_over_q if \code{parallel} is TRUE then paralellize over quantiles?
-#' Defalts to FALSE i.e."kfold".
-#' @param Sort \code{boolean} Sort quantiles using \code{SortQuantiles()}?
-#' @param SortLimits \code{Limits} argument to be passed to \code{SortQuantiles()}. Constrains quantiles to upper and lower limits given by \code{list(U=upperlim,L=lowerlim)}.
-#' @details The returned predictive quantiles are those produced out-of-sample for each
-#' cross-validation fold (using models trained on the remaining folds but not "Test" data).
-#' Predictive quantiles corresponding to "Test" data are produced using models trained on all
-#' non-test data.
-#' @return Quantile forecasts in a \code{MultiQR} object.
-#' @keywords Quantile Regression
-#' @export
-MQR_gbm <- function(data,
-                    formula,
-                    quantiles=c(0.25,0.5,0.75),
-                    CVfolds=NULL,
-                    gbm_params=list(...),
-                    perf.plot=F,
-                    parallel = F,
-                    pred_ntree = NULL,
-                    cores = NULL,
-                    pckgs = NULL,
-                    para_over_q = FALSE,
-                    Sort=T,
-                    SortLimits = NULL){
-  
-  
-  warning("function depreciated and may be removed in future updates. Update to ProbCast::qreg_gbm()")
-  
-  if(is.null(CVfolds)){
-    
-    fold_cont <- "kfold"
-    
-  } else{
-    
-    fold_cont <- CVfolds
-    
-  }
-  
-  
-  if(parallel){
-    
-    if(isFALSE(para_over_q)){
-      
-      warning("parallelism is now only spported over quantiles. Calculating over quantiles...")
-      
-    }
-  
-  
-    do.call(qreg_gbm,c(list(data = data, 
-                            formula = formula,
-                            quantiles=quantiles,
-                            cv_folds=fold_cont,
-                            perf.plot=perf.plot,
-                            pred_ntree = pred_ntree,
-                            cores = cores,
-                            pckgs = pckgs,
-                            sort = Sort,
-                            sort_limits= SortLimits,
-                            save_models_path=NULL,
-                            only_mqr = TRUE),
-                       gbm_params))
-    
-    
-  } else{
-    
-    
-    do.call(qreg_gbm,c(list(data = data, 
-                            formula = formula,
-                            quantiles=quantiles,
-                            cv_folds=fold_cont,
-                            perf.plot=perf.plot,
-                            pred_ntree = pred_ntree,
-                            cores = 1,
-                            pckgs = pckgs,
-                            sort = Sort,
-                            sort_limits= SortLimits,
-                            save_models_path=NULL,
-                            only_mqr = TRUE),
-                       gbm_params))
-    
-    
-    
-  }
-    
-  
-  
-}
-
 
 
 
 #' Predict method for Multiple Quantile Regression from Gradient Boosted Decision Trees.
 #'
 #' This function returns multiple quantile predictions as an object of class \code{MultiQR}
-#' based on a ProbCast gbm fitted model. S3 Method for for \code{qreg_gbm} objects
+#' based on a ProbCast lightgbm fitted model. S3 Method for for \code{qreg_lightgbm} objects
 #' 
-#' @author Ciaran Gilbert, \email{ciaran.gilbert@@strath.ac.uk}
-#' @param object 	object of class \code{qreg_gbm} obtained from the function \code{qreg_gbm()}.
-#' @param newdata data.frame of observations for which to make predictions
+#' @author Ciaran Gilbert, \email{ciaran.gilbert@@strath.ac.uk}; Rosemary Tawn, \email{rosemary.tawn@@strath.ac.uk}
+#' @param object 	object of class \code{qreg_lightgbm} obtained from the function \code{qreg_lightgbm()}.
+#' @param newdata matrix of observations for which to make predictions; only columns used as model inputs must be included.
 #' @param quantiles the quantiles to predict. Default is all the quantiles present in \code{object}
 #' @param model_name the name of the model to be used for prediction.
 #' Unless specified, \code{object$default_model} is used.
-#' @param pred_ntree predict using a user-specified tree.
-#' If NULL \code{gbm::gbm.perf()} is used to estimate the best tree via out-of-the-bag estimates,
-#' unless internal gbm cross-validation folds are specified via the \code{...} argument in \code{qreg_gbm()}.
-#' @param perf.plot plot GBM performance if \code{pred_ntree = NULL}?
 #' @param sort sort quantiles using \code{SortQuantiles()}?
 #' @param sort_limits \code{Limits} argument to be passed to \code{SortQuantiles()}. Constrains quantiles to upper and 
 #' lower limits given by \code{list(U=upperlim,L=lowerlim)}.
@@ -349,19 +220,17 @@ MQR_gbm <- function(data,
 #' @return Quantile forecasts in a \code{MultiQR} object.
 #' @keywords Quantile Regression
 #' @export
-predict.qreg_gbm <- function(object,
+predict.qreg_lgbm <- function(object,
                              newdata = NULL,
                              quantiles = NULL,
                              model_name = NULL,
-                             pred_ntree = NULL,
-                             perf.plot = FALSE,
                              sort = T,
                              sort_limits = NULL,
                              ...) {
   
   
-  
-  if(class(object)!="qreg_gbm"){stop("object of wrong class, expecting \"qreg_gbm\"")}
+  require(lightgbm)
+  if(class(object)!="qreg_lightgbm"){stop("object of wrong class, expecting \"qreg_lightgbm\"")}
   
   
   if(is.null(model_name)){
@@ -391,18 +260,11 @@ predict.qreg_gbm <- function(object,
   pred <- data.frame(sapply(quantiles,function(q){
     
     ### Save out-of-sample predictions
-    if(is.null(pred_ntree)){
-      gbm::predict.gbm(object$models[[model_name]][[q]],
-                       newdata = newdata,
-                       n.trees = gbm::gbm.perf(object$models[[model_name]][[q]],plot.it = perf.plot))
-    } else{
-      
-      gbm::predict.gbm(object$models[[model_name]][[q]],
-                       newdata = newdata,
-                       n.trees = pred_ntree)
-      
-    }
-    
+    test <- lightgbm::predict.lgb.Booster(object$models[[model_name]][[q]],
+                                          data = newdata)
+    test <- predict(object$models[[model_name]][[q]],
+                                          data = newdata)
+ 
     
   }))
   
