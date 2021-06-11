a2d4c435e1c22744f440a011e88a1ccb4714bda2
Ciaran <41995662+ciaran-g@users.noreply.github.com>
2021-06-11 12:30:44 +0100

    Update testing_lightgbm_inparallel.R
    @rosemary-t

--- R/testing_lightgbm_inparallel.R
+++ R/testing_lightgbm_inparallel.R
@@ -4,7 +4,8 @@ require(ProbCast)
 require(gbm)
 require(rstudioapi)
 
-setwd(dirname(getActiveDocumentContext()$path))
+# setwd(dirname(getActiveDocumentContext()$path))
+setwd("C:/Users/Ciaran/Desktop/")
 ## testing out putting lightgbm in parallel.. what is making it fail?
 
 
@@ -47,13 +48,13 @@ output <- foreach::foreach(q = quantiles,.packages ="lightgbm",.options.snow = o
     X <- as.matrix(train_data[, features])
     dataset <- lgb.Dataset(data=X, label=train_data[,response])
     #lgb_params <- list(..., num_iterations=num_iterations, objective="quantile")
-    #lgb_params <- list(objective="quantile", num_iterations=100)
-    lgb_params <- list(objective="regression", num_iterations=100)
-    #lgb_model = lgb.train(params=lgb_params, data=dataset, alpha=q, num_threads=1)
-    lgb_model = lgb.train(params=lgb_params, data=dataset, num_threads=1)
-    model_file = tempfile(fileext=paste0(fold, ".txt"))
+    lgb_params <- list(objective="quantile", num_iterations=100)
+    # lgb_params <- list(objective="regression", num_iterations=100)
+    lgb_model = lgb.train(params=lgb_params, data=dataset, alpha=q, num_threads=1)
+    # lgb_model = lgb.train(params=lgb_params, data=dataset, num_threads=1)
+    model_file = paste0(fold,"_",q,".txt")
     lgb.save(lgb_model, model_file)
-    #models[[fold]] <- lgb_model
+    models[[fold]] <- lgb_model
     
     #test_data <- Wind[Wind$kfold==fold, features]
     #models[[paste0(fold,"_fc")]] <- predict(lgb_model, as.matrix(test_data))
@@ -61,7 +62,7 @@ output <- foreach::foreach(q = quantiles,.packages ="lightgbm",.options.snow = o
     
   }
   
-  return (models)
+  return(models)
   
 }
 
@@ -69,7 +70,24 @@ close(pb)
 parallel::stopCluster(cl)
 names(output) <- paste0(quantiles)
 
+#  crashing just like before!
+# test_data <- as.matrix(Wind[Wind$kfold=="Fold 3", features])
+# lightgbm:::predict.lgb.Booster(output$`0.05`$`Fold 3`, test_data,num_iteration = 100)
 
+
+load_booster <- lightgbm::lgb.load(filename = "Fold 3_0.05.txt")
+test_data <- as.matrix(Wind[Wind$kfold=="Fold 3", features])
+plot(head(lightgbm:::predict.lgb.Booster(load_booster, test_data),48),ylim=c(0,1),type="l")
+
+
+load_booster <- lightgbm::lgb.load(filename = "Fold 3_0.95.txt")
+test_data <- as.matrix(Wind[Wind$kfold=="Fold 3", features])
+lines(lightgbm:::predict.lgb.Booster(load_booster, test_data),col="red")
+
+
+
+
+########################################## this works for me
 cl <- parallel::makeCluster(cores)
 doSNOW::registerDoSNOW(cl)
 #set up progress bar
@@ -83,15 +101,93 @@ forecasts <- foreach::foreach(q = quantiles,.packages ="lightgbm",.options.snow 
   foldresults <- list()
   ## now make predictions..
   for (fold in unique(Wind$kfold)){
-    test_data <- Wind[Wind$kfold==fold, features]
-    foldresults[[fold]] <- try(lightgbm:::predict.lgb.Booster(output[[q]][[fold]], as.matrix(test_data)))
+    # test_data <- Wind[Wind$kfold==fold, features]
+    # foldresults[[fold]] <- try(lightgbm:::predict.lgb.Booster(output[[q]][[fold]], as.matrix(test_data)))
+    
+    
+    load_booster <- lgb.load(filename = paste0(fold,"_",q,".txt"))
+    test_data <- as.matrix(Wind[Wind$kfold==fold, features])
+    foldresults[[fold]] <- predict(load_booster, test_data)
+    
+    
+    
   }
   
-  #test <- unlist(foldresults)
-  #return(test)
+  return(foldresults)
 }
 close(pb)
 parallel::stopCluster(cl)
+names(forecasts) <- paste0(quantiles)
+
+
+
+output  <- data.frame(matrix(NA,ncol = length(quantiles), nrow = nrow(Wind)))
+colnames(output) <- paste0("q",100*quantiles)
+
+for(q in quantiles){
+  for(fold in unique(Wind$kfold)){
+    
+    output[Wind$kfold==fold,paste0("q",q*100)] <-forecasts[[as.character(q)]][[fold]]
+    
+  }
+}
+# class of new cv object
+class(output) <- c("MultiQR","data.frame")
+
+
+plot(output[1:100,])
+
+
+
+
+
+# here's my session info --->
+sessionInfo()
+# R version 4.0.2 (2020-06-22)
+# Platform: x86_64-w64-mingw32/x64 (64-bit)
+# Running under: Windows 10 x64 (build 19041)
+# 
+# Matrix products: default
+# 
+# locale:
+#   [1] LC_COLLATE=English_United Kingdom.1252 
+# [2] LC_CTYPE=English_United Kingdom.1252   
+# [3] LC_MONETARY=English_United Kingdom.1252
+# [4] LC_NUMERIC=C                           
+# [5] LC_TIME=English_United Kingdom.1252    
+# 
+# attached base packages:
+#   [1] parallel  splines   stats     graphics  grDevices utils    
+# [7] datasets  methods   base     
+# 
+# other attached packages:
+#   [1] rstudioapi_0.11     gbm_2.1.8           ProbCast_0.0.0.9000
+# [4] quantreg_5.75       SparseM_1.78        mgcv_1.8-31        
+# [7] evgam_0.1.4         fst_0.9.4           mvnfast_0.2.5.1    
+# [10] gamboostLSS_2.0-1.1 mboost_2.9-3        stabs_0.6-3        
+# [13] doSNOW_1.0.19       snow_0.4-3          iterators_1.0.13   
+# [16] foreach_1.5.1       gamlss_5.2-0        nlme_3.1-148       
+# [19] gamlss.dist_5.1-7   MASS_7.3-51.6       gamlss.data_5.1-4  
+# [22] data.table_1.14.0  
+# 
+# loaded via a namespace (and not attached):
+#   [1] Rcpp_1.0.6         compiler_4.0.2     tools_4.0.2       
+# [4] partykit_1.2-10    rpart_4.1-15       jsonlite_1.7.0    
+# [7] lattice_0.20-41    Matrix_1.2-18      mvtnorm_1.1-1     
+# [10] libcoin_1.0-6      MatrixModels_0.4-1 grid_4.0.2        
+# [13] R6_2.4.1           survival_3.1-12    lightgbm_3.2.1    
+# [16] Formula_1.2-4      conquer_1.0.2      matrixStats_0.57.0
+# [19] codetools_0.2-16   nnls_1.4           quadprog_1.5-8    
+# [22] inum_1.0-1    
+
+
+
+
+
+
+
+
+
 
 fold <- unique(Wind$kfold)[1]
 test_data <- Wind[Wind$kfold==fold, features]
